9e38ea5e386d5cae787f9ba9f8fffae49343d291
Bhaktij Koli <bhaktijkoli121@gmail.com>
2022-06-21 04:14:27 +0530

    feat: added export and import

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,78 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import ImportExportIcon from "@mui/icons-material/ImportExport";
+import SaveAltIcon from "@mui/icons-material/SaveAlt";
+import * as React from "react";
+
+const Navbar = ({
+  importJSON,
+  exportToJSON,
+  exportToHTML,
+  handleCopyHTMLToClipboard,
+}) => {
+  const [exportAnchorEl, setExportAnchorEl] = React.useState(null);
+  const handleExportClick = React.useCallback((event) => {
+    setExportAnchorEl(event.currentTarget);
+  }, []);
+  const handleExportToJSON = React.useCallback(() => {
+    exportToJSON();
+  }, [exportToJSON]);
+  const handleExportToHTML = React.useCallback(() => {
+    exportToHTML();
+  }, [exportToHTML]);
+  return (
+    <AppBar sx={{ backgroundColor: "#FFF", color: "#130f40" }} position="fixed">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Email Designer
+        </Typography>
+        <Box>
+          <Button
+            sx={{ mr: 2 }}
+            onClick={importJSON}
+            variant="contained"
+            startIcon={<ImportExportIcon />}
+          >
+            Import
+          </Button>
+          <Button
+            onClick={handleExportClick}
+            variant="contained"
+            startIcon={<SaveAltIcon />}
+          >
+            Export
+          </Button>
+        </Box>
+      </Toolbar>
+      <Menu
+        id="export-menu"
+        anchorEl={exportAnchorEl}
+        open={exportAnchorEl !== null}
+        onClose={() => setExportAnchorEl(null)}
+        MenuListProps={{
+          "aria-labelledby": "export-button",
+        }}
+      >
+        <MenuItem onClick={handleExportToJSON}>Export JSON</MenuItem>
+        <MenuItem onClick={handleExportToHTML}>Export HTML</MenuItem>
+        <MenuItem
+          onClick={() => {
+            setExportAnchorEl(null);
+            handleCopyHTMLToClipboard();
+          }}
+        >
+          Copy HTML to Clipboard
+        </MenuItem>
+      </Menu>
+    </AppBar>
+  );
+};
+
+export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,82 @@
+import * as React from "react";
+import { Navbar } from "../components";
+import EmailEditor from "react-email-editor";
+import { Box } from "@mui/material";
+import FileSaver from "file-saver";
+import { toast } from "react-toastify";
+
 const HomePage = () => {
-  return <h1>Hello World</h1>;
+  const emailEditorRef = React.useRef(null);
+
+  const importJSON = () => {
+    const inputElement = document.createElement("input");
+    inputElement.type = "file";
+    inputElement.accept = "text/json,text/txt";
+    inputElement.style.display = "none";
+    inputElement.addEventListener("change", (e) => {
+      const input = e.target;
+      if (input.files && input.files.length > 0) {
+        // Get File
+        const file = input.files[0];
+        const reader = new FileReader();
+        reader.addEventListener("load", (event) => {
+          const result = event.target?.result;
+          try {
+            const design = JSON.parse(String(result));
+            emailEditorRef.current?.loadDesign(design);
+          } catch (error) {}
+        });
+        reader.readAsText(file);
+      }
+      inputElement.remove();
+    });
+    document.body.append(inputElement);
+    inputElement.click();
+  };
+
+  const exportToJSON = () => {
+    emailEditorRef.current?.saveDesign((data) => {
+      const blob = new Blob([JSON.stringify(data)], {
+        type: "text/plain;charset=utf-8",
+      });
+      FileSaver.saveAs(blob, "design.json");
+    });
+  };
+
+  const exportToHTML = () => {
+    emailEditorRef.current?.exportHtml((data) => {
+      const { html } = data;
+      const blob = new Blob([html], { type: "text/plain;charset=utf-8" });
+      FileSaver.saveAs(blob, "design.html");
+    });
+  };
+
+  const handleCopyHTMLToClipboard = () => {
+    emailEditorRef.current?.exportHtml((data) => {
+      const { html } = data;
+      navigator.clipboard.writeText(html).then(() => {
+        toast("Copied to Clipboard");
+      });
+    });
+  };
+
+  return (
+    <>
+      <Navbar
+        importJSON={importJSON}
+        exportToJSON={exportToJSON}
+        exportToHTML={exportToHTML}
+        handleCopyHTMLToClipboard={handleCopyHTMLToClipboard}
+      />
+      <Box sx={{ mt: "64px" }}>
+        <EmailEditor
+          ref={emailEditorRef}
+          editorId="editor"
+          style={{ height: "calc(100vh - 64px)" }}
+        />
+      </Box>
+    </>
+  );
 };
 
 export default HomePage;
